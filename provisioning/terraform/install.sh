2ed2e38f6bce596c598dac3368d66f2101c4a4ea
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-05 15:59:08 +0530

    Providing ability to specify versions for kubectl iam-authenticator and terraform

--- provisioning/terraform/install.sh
+++ provisioning/terraform/install.sh
@@ -2,7 +2,7 @@
 
 figlet terraform
 
-curl -LO https://raw.github.com/robertpeteuil/terraform-installer/master/terraform-install.sh
+curl -LO https://raw.github.com/robertpeteuil/terraform-installer/master/terraform-install.sh -i $1
 chmod +x terraform-install.sh
 ./terraform-install.sh
 cp  /usr/local/bin/terraform /home/vagrant/bin/terraform
