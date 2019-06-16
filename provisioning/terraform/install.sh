3f5cad2342ce0e8d56e023523be4c12271356faf
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-16 14:15:13 +0530

    Update install.sh

--- provisioning/terraform/install.sh
+++ provisioning/terraform/install.sh
@@ -2,7 +2,7 @@
 
 figlet terraform
 
-curl -LO https://raw.github.com/robertpeteuil/terraform-installer/master/terraform-install.sh -i $1
+curl -LO https://raw.github.com/robertpeteuil/terraform-installer/master/terraform-install.sh
 chmod +x terraform-install.sh
-./terraform-install.sh
+./terraform-install.sh  -i $1
 cp  /usr/local/bin/terraform /home/vagrant/bin/terraform
