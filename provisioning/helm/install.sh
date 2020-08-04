e153b70f9ae525ea0be61dc668dcc6912a603c0c
subhakarkotta <subhakarkotta@gmail.com>
2020-08-04 09:48:55 +0530

    Update install.sh

--- provisioning/helm/install.sh
+++ provisioning/helm/install.sh
@@ -3,11 +3,8 @@
 figlet helm
 
 export PATH=/usr/local/bin:/home/vagrant/bin:$PATH
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+curl ttps://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
 chmod +x get_helm.sh
-./get_helm.sh -v v$1
+./get_helm.sh
 
 cp  /usr/local/bin/helm /home/vagrant/bin/helm
-cp  /usr/local/bin/tiller /home/vagrant/bin/tiller
-
-helm init --client-only
